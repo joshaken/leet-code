63e524e7e4c8e5ec4055b5c86eb09de92ff88d2e
zjq <jqchu96@outlook.com>
2026-02-08 19:58:18 +0900

    dp

--- src/main/java/com/leetcode/dp/ClimbingStairs70.java
+++ src/main/java/com/leetcode/dp/ClimbingStairs70.java
@@ -17,4 +17,15 @@ public class ClimbingStairs70 {
         }
         return cur;
     }
+
+    public int climbStairs2(int n) {
+
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
 }
